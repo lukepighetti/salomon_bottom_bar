b5b53c44f5dae584fd2e57afdc054548323d1f10
lukepighetti <lukepighetti@gmail.com>
2020-05-17 15:07:33 -0400

    generate project and wire up package

---
+++ example/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'salomon_bottom_bar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("salomon_bottom_bar"),
+        ),
+        body: Center(
+          child: SalomonBottomBar(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/salomon_bottom_bar.dart
@@ -0,0 +1,15 @@
+library salomon_bottom_bar;
+
+import 'package:flutter/material.dart';
+
+class SalomonBottomBar extends StatefulWidget {
+  @override
+  _SalomonBottomBarState createState() => _SalomonBottomBarState();
+}
+
+class _SalomonBottomBarState extends State<SalomonBottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("SalomonBottomBar");
+  }
+}
