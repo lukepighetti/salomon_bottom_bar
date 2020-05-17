919c5670bad569b481e13d798288e363c8abff95
lukepighetti <lukepighetti@gmail.com>
2020-05-17 16:14:27 -0400

    naiive implementation

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,7 +5,21 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _selectedTab = _SelectedTab.home;
+
+  void _handleValueTapped(_SelectedTab e) {
+    setState(() {
+      _selectedTab = e;
+      print(e);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,9 +33,47 @@ class MyApp extends StatelessWidget {
           title: Text("salomon_bottom_bar"),
         ),
         body: Center(
-          child: SalomonBottomBar(),
+          child: SalomonBottomBar<_SelectedTab>(
+            selectedValue: _selectedTab,
+            onValueTapped: _handleValueTapped,
+            tabs: [
+              /// Home
+              SBBTab(
+                icon: Icon(Icons.home),
+                title: "Home",
+                color: Colors.purple,
+                value: _SelectedTab.home,
+              ),
+
+              /// Likes
+              SBBTab(
+                icon: Icon(Icons.favorite_border),
+                title: "Likes",
+                color: Colors.pink,
+                value: _SelectedTab.likes,
+              ),
+
+              /// Search
+              SBBTab(
+                icon: Icon(Icons.search),
+                title: "Search",
+                color: Colors.orange,
+                value: _SelectedTab.search,
+              ),
+
+              /// Profile
+              SBBTab(
+                icon: Icon(Icons.person),
+                title: "Profile",
+                color: Colors.teal,
+                value: _SelectedTab.profile,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+enum _SelectedTab { home, likes, search, profile }
--- lib/salomon_bottom_bar.dart
+++ lib/salomon_bottom_bar.dart
@@ -2,14 +2,107 @@ library salomon_bottom_bar;
 
 import 'package:flutter/material.dart';
 
-class SalomonBottomBar extends StatefulWidget {
-  @override
-  _SalomonBottomBarState createState() => _SalomonBottomBarState();
-}
+import 'implicitly_animated_builder.dart';
 
-class _SalomonBottomBarState extends State<SalomonBottomBar> {
+class SalomonBottomBar<T> extends StatelessWidget {
+  /// A list of tabs to display, ie `Home`, `Likes`, etc
+  final List<SBBTab<T>> tabs;
+
+  /// The intial tab to highly, based on matching value.
+  final T selectedValue;
+
+  /// Returns the value of the tab that was tapped.
+  final Function(T) onValueTapped;
+
+  SalomonBottomBar({
+    Key key,
+    @required this.tabs,
+    this.selectedValue,
+    this.onValueTapped,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Text("SalomonBottomBar");
+    return Material(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            for (final tab in tabs)
+              ImplicitlyAnimatedBuilder(
+                isActive: tab.value == selectedValue,
+                curve: Curves.easeOut,
+                duration: Duration(milliseconds: 250),
+                builder: (context, t) {
+                  return Material(
+                    color: Color.lerp(
+                        Colors.transparent, tab.color.withOpacity(0.10), t),
+                    shape: StadiumBorder(),
+                    child: InkWell(
+                      onTap: () => onValueTapped?.call(tab.value),
+                      customBorder: StadiumBorder(),
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 16) -
+                                EdgeInsets.only(right: 16 * t),
+                        child: Row(
+                          children: [
+                            IconTheme(
+                              data: IconThemeData(
+                                color: Color.lerp(Colors.black, tab.color, t),
+                                size: 24,
+                              ),
+                              child: tab.icon ?? SizedBox.shrink(),
+                            ),
+                            ClipRect(
+                              child: SizedBox(
+                                height: 20,
+                                child: Align(
+                                  alignment: Alignment(-0.2, 0.0),
+                                  widthFactor: t,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 8.0, right: 16),
+                                    child: Text(
+                                      tab.title,
+                                      style: TextStyle(
+                                        color: Color.lerp(
+                                            Colors.transparent, tab.color, t),
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+/// A tab to display in a [SalomonBottomBar]
+class SBBTab<T> {
+  /// A value to attach to this tab item.
+  final T value;
+
+  /// An icon to display.
+  final Widget icon;
+
+  /// Text to display, ie `Home`
+  final String title;
+
+  /// A primary color to use for this tab.
+  final Color color;
+
+  SBBTab({this.value, this.icon, this.title, this.color});
 }
