0c4728c6622696430e0c81cc180da4e1414574f4
lukepighetti <lukepighetti@gmail.com>
2021-03-09 13:12:44 -0500

    clean up example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,69 +6,61 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
+  static final title = 'salomon_bottom_bar';
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  var _selectedTab = _SelectedTab.home;
-
-  void _handleIndexChanged(int i) {
-    setState(() {
-      _selectedTab = _SelectedTab.values[i];
-    });
-  }
+  var _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'salomon_bottom_bar',
+      title: MyApp.title,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text("salomon_bottom_bar"),
+          title: Text(MyApp.title),
         ),
-        body: Center(
-          child: SalomonBottomBar(
-            currentIndex: _SelectedTab.values.indexOf(_selectedTab),
-            onTap: _handleIndexChanged,
-            items: [
-              /// Home
-              SalomonBottomBarItem(
-                icon: Icon(Icons.home),
-                title: Text("Home"),
-                selectedColor: Colors.purple,
-              ),
+        bottomNavigationBar: SalomonBottomBar(
+          currentIndex: _currentIndex,
+          onTap: (i) => setState(() => _currentIndex = i),
+          items: [
+            /// Home
+            SalomonBottomBarItem(
+              icon: Icon(Icons.home),
+              title: Text("Home"),
+              selectedColor: Colors.purple,
+            ),
 
-              /// Likes
-              SalomonBottomBarItem(
-                icon: Icon(Icons.favorite_border),
-                title: Text("Likes"),
-                selectedColor: Colors.pink,
-              ),
+            /// Likes
+            SalomonBottomBarItem(
+              icon: Icon(Icons.favorite_border),
+              title: Text("Likes"),
+              selectedColor: Colors.pink,
+            ),
 
-              /// Search
-              SalomonBottomBarItem(
-                icon: Icon(Icons.search),
-                title: Text("Search"),
-                selectedColor: Colors.orange,
-              ),
+            /// Search
+            SalomonBottomBarItem(
+              icon: Icon(Icons.search),
+              title: Text("Search"),
+              selectedColor: Colors.orange,
+            ),
 
-              /// Profile
-              SalomonBottomBarItem(
-                icon: Icon(Icons.person),
-                title: Text("Profile"),
-                selectedColor: Colors.teal,
-              ),
-            ],
-          ),
+            /// Profile
+            SalomonBottomBarItem(
+              icon: Icon(Icons.person),
+              title: Text("Profile"),
+              selectedColor: Colors.teal,
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-enum _SelectedTab { home, likes, search, profile }
