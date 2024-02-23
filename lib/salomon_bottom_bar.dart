abfcf8e4d71dba03eba81f2581ef2f0b111b1df4
Shafin Murani <66113323+shafinmurani@users.noreply.github.com>
2024-02-24 01:20:27 +0530

    Update salomon_bottom_bar.dart for personal usage
    Added borders to icons, as per a personal client request.

--- lib/salomon_bottom_bar.dart
+++ lib/salomon_bottom_bar.dart
@@ -7,6 +7,8 @@ class SalomonBottomBar extends StatelessWidget {
   SalomonBottomBar({
     Key? key,
     required this.items,
+    this.border,
+    this.borderRadius = 10,
     this.backgroundColor,
     this.currentIndex = 0,
     this.onTap,
@@ -22,6 +24,12 @@ class SalomonBottomBar extends StatelessWidget {
 
   /// A list of tabs to display, ie `Home`, `Likes`, etc
   final List<SalomonBottomBarItem> items;
+
+  /// border for unselected items
+  final Border? border;
+
+  /// border radius
+  final double borderRadius;
 
   /// The tab to display.
   final int currentIndex;
@@ -100,58 +108,72 @@ class SalomonBottomBar extends StatelessWidget {
                       highlightColor: _selectedColor.withOpacity(0.1),
                       splashColor: _selectedColor.withOpacity(0.1),
                       hoverColor: _selectedColor.withOpacity(0.1),
-                      child: Padding(
-                        padding: itemPadding -
-                            (Directionality.of(context) == TextDirection.ltr
-                                ? EdgeInsets.only(right: itemPadding.right * t)
-                                : EdgeInsets.only(left: itemPadding.left * t)),
-                        child: Row(
-                          children: [
-                            IconTheme(
-                              data: IconThemeData(
-                                color: Color.lerp(
-                                    _unselectedColor, _selectedColor, t),
-                                size: 24,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: items.indexOf(item) == currentIndex
+                                ? Colors.transparent
+                                : backgroundColor,
+                            border: items.indexOf(item) == currentIndex
+                                ? null
+                                : border,
+                            borderRadius: items.indexOf(item) == currentIndex
+                                ? BorderRadius.zero
+                                : BorderRadius.circular(borderRadius)),
+                        child: Padding(
+                          padding: itemPadding -
+                              (Directionality.of(context) == TextDirection.ltr
+                                  ? EdgeInsets.only(
+                                      right: itemPadding.right * t)
+                                  : EdgeInsets.only(
+                                      left: itemPadding.left * t)),
+                          child: Row(
+                            children: [
+                              IconTheme(
+                                data: IconThemeData(
+                                  color: Color.lerp(
+                                      _unselectedColor, _selectedColor, t),
+                                  size: 24,
+                                ),
+                                child: items.indexOf(item) == currentIndex
+                                    ? item.activeIcon ?? item.icon
+                                    : item.icon,
                               ),
-                              child: items.indexOf(item) == currentIndex
-                                  ? item.activeIcon ?? item.icon
-                                  : item.icon,
-                            ),
-                            ClipRect(
-                              clipBehavior: Clip.antiAlias,
-                              child: SizedBox(
-                                /// TODO: Constrain item height without a fixed value
-                                ///
-                                /// The Align property appears to make these full height, would be
-                                /// best to find a way to make it respond only to padding.
-                                height: 20,
-                                child: Align(
-                                  alignment: Alignment(-0.2, 0.0),
-                                  widthFactor: t,
-                                  child: Padding(
-                                    padding: Directionality.of(context) ==
-                                            TextDirection.ltr
-                                        ? EdgeInsets.only(
-                                            left: itemPadding.left / 2,
-                                            right: itemPadding.right)
-                                        : EdgeInsets.only(
-                                            left: itemPadding.left,
-                                            right: itemPadding.right / 2),
-                                    child: DefaultTextStyle(
-                                      style: TextStyle(
-                                        color: Color.lerp(
-                                            _selectedColor.withOpacity(0.0),
-                                            _selectedColor,
-                                            t),
-                                        fontWeight: FontWeight.w600,
+                              ClipRect(
+                                clipBehavior: Clip.antiAlias,
+                                child: SizedBox(
+                                  /// TODO: Constrain item height without a fixed value
+                                  ///
+                                  /// The Align property appears to make these full height, would be
+                                  /// best to find a way to make it respond only to padding.
+                                  height: 20,
+                                  child: Align(
+                                    alignment: Alignment(-0.2, 0.0),
+                                    widthFactor: t,
+                                    child: Padding(
+                                      padding: Directionality.of(context) ==
+                                              TextDirection.ltr
+                                          ? EdgeInsets.only(
+                                              left: itemPadding.left / 2,
+                                              right: itemPadding.right)
+                                          : EdgeInsets.only(
+                                              left: itemPadding.left,
+                                              right: itemPadding.right / 2),
+                                      child: DefaultTextStyle(
+                                        style: TextStyle(
+                                          color: Color.lerp(
+                                              _selectedColor.withOpacity(0.0),
+                                              _selectedColor,
+                                              t),
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                        child: item.title,
                                       ),
-                                      child: item.title,
                                     ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
